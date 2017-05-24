6e3d3d8cb92468b79ac19a82c653531da04e0743
Ryan Currington <rcurrington@users.noreply.github.com>
2017-05-24 13:34:30 -0400

    Update Redis.php
    Magento local.xml files allows you to set the db, if you don't pass it into the select, the code fails to return data.

--- app/code/community/Pulsestorm/Crossareasession/Model/Loader/Redis.php
+++ app/code/community/Pulsestorm/Crossareasession/Model/Loader/Redis.php
@@ -7,18 +7,21 @@ class Pulsestorm_Crossareasession_Model_Loader_Redis extends Pulsestorm_Crossare
     const XML_PATH_PASS            = 'global/redis_session/password';
     const XML_PATH_TIMEOUT         = 'global/redis_session/timeout';
     const XML_PATH_PERSISTENT      = 'global/redis_session/persistent';
+    const XML_PATH_DB              = 'global/redis_session/db';
 
     public function _load($session_id){
         $host    = (string)   (Mage::getConfig()->getNode(self::XML_PATH_HOST) ?: '127.0.0.1');
         $port    = (int)      (Mage::getConfig()->getNode(self::XML_PATH_PORT) ?: '6379');
         $pass    = (string)   (Mage::getConfig()->getNode(self::XML_PATH_PASS) ?: '');
         $timeout = (float) (Mage::getConfig()->getNode(self::XML_PATH_TIMEOUT) ?: '2.5');
+        $db      = (int)      (Mage::getConfig()->getNode(self::XML_PATH_DB) ?: '0');
 
         $_redis = new Credis_Client($host, $port, $timeout);
         if (!empty($pass)) {
             $_redis->auth($pass);
         }
         $_redis->connect(); // connect to redis
+        $_redis->select($db);
         // replace sess_session_id with session id you want to read.
         $sessionData = $_redis->hGet('sess_'.$session_id, 'data');
         
